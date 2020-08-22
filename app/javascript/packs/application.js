3f4cce6ff83bf28f7b82f954350d83375cf9cfbb
Rocky Harding <purnell.j@me.com>
2020-08-22 07:28:38 +0100

    Added materialize theme including relevant CSS and JS packages.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,13 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'materialize-css/dist/js/materialize'
+
+$(document).on("turbolinks:load", function() {
+    $(".dropdown-trigger").dropdown();
+    $('.sidenav').sidenav();
+})
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
